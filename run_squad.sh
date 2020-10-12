f9b9d91db9d9d89dbe3e0f75ec9262b795a9a814
pengy <pengyu1215@qq.com>
2020-10-12 20:12:08 +0800

    run_squad.sh更新配置文件为2.0版本的squad数据集

--- run_squad.sh
+++ run_squad.sh
@@ -6,8 +6,8 @@ python run_squad.py \
   --do_train \
   --do_eval \
   --do_lower_case \
-  --train_file $SQUAD_DIR/train-v1.1.json \
-  --predict_file $SQUAD_DIR/dev-v1.1.json \
+  --train_file $SQUAD_DIR/train-v2.0.json \
+  --predict_file $SQUAD_DIR/dev-v2.0.json \
   --per_gpu_train_batch_size 12 \
   --learning_rate 3e-5 \
   --num_train_epochs 2.0 \
